70ec63cb8554857ecd668533dad2cb5f0b807220
Adrian WEAVER <aweaver@student.42.fr>
2022-11-09 10:25:11 +0100

    ADD SETTERS AND GETTERS:
    Adding setters and getters for contact class.

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:57:07 by aweaver           #+#    #+#             */
-/*   Updated: 2022/11/07 16:47:28 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/11/09 10:24:56 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,47 @@ Contact::~Contact(void)
 	return ;
 }
 
-void	Contact::_set_first_name(std::string first_name)
+void	Contact::set_first_name(std::string first_name)
 {
-	this->first_name = first_name;
+	this->_first_name = first_name;
 }
 
-void	Contact::_set_last_name(std::string last_name)
+void	Contact::set_last_name(std::string last_name)
 {
-	this->last_name = last_name;
+	this->_last_name = last_name;
 }
 
-void	Contact::_set_nickname(std::string nickname)
+void	Contact::set_nickname(std::string nickname)
 {
-	this->nickname = nickname;
+	this->_nickname = nickname;
 }
 
-void	Contact::_set_darkest_secret(std::string darkest_secret)
+void	Contact::set_darkest_secret(std::string darkest_secret)
 {
-	this->darkest_secret = darkest_secret;
+	this->_darkest_secret = darkest_secret;
+}
+
+std::string	Contact::get_first_name(void)
+{
+	return (this->_first_name);
+}
+
+std::string	Contact::get_last_name(void)
+{
+	return (this->_last_name);
+}
+
+std::string	Contact::get_nickname(void)
+{
+	return (this->_nickname);
+}
+
+std::string	Contact::get_phone_number(void)
+{
+	return (this->_phone_number);
+}
+
+std::string	Contact::get_darkest_secret(void)
+{
+	return (this->_darkest_secret);
 }
--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 08:15:23 by aweaver           #+#    #+#             */
-/*   Updated: 2022/11/07 16:48:12 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/11/09 10:24:53 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,23 @@ class Contact
 	public:
 		Contact(void);
 		~Contact(void);
-		std::string		first_name;
-		std::string		last_name;
-		std::string		nickname;
-		std::string		phone_number;
-		std::string		darkest_secret;
-		void	_set_first_name(std::string first_name);
-		void	_set_last_name(std::string last_name);
-		void	_set_nickname(std::string nickname);
-		void	_set_phonenumber(std::string phone_number);
-		void	_set_darkest_secret(std::string darkest_secret);
+		void	set_first_name(std::string first_name);
+		void	set_last_name(std::string last_name);
+		void	set_nickname(std::string nickname);
+		void	set_phonenumber(std::string phone_number);
+		void	set_darkest_secret(std::string darkest_secret);
+		std::string	get_first_name(void);
+		std::string	get_last_name(void);
+		std::string	get_nickname(void);
+		std::string	get_phone_number(void);
+		std::string	get_darkest_secret(void);
 	protected:
 	private:
+		std::string		_first_name;
+		std::string		_last_name;
+		std::string		_nickname;
+		std::string		_phone_number;
+		std::string		_darkest_secret;
 };
 
 #endif
